c207733656ff0f97b4a9045caf80e658707adf10
Saac Sornchai <sornchai.la@ku.th>
2024-01-29 16:10:19 +0700

    update code-02.cpp on branch feature2

--- code-02.cpp
+++ code-02.cpp
@@ -4,5 +4,5 @@ using namespace std;
 int main() {
     // cout : console output
                            // endl : end line '\n'
-    cout << "Hello Git" << endl;
+    cout << "Hello C++" << endl;
 }
